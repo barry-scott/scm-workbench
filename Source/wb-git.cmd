8998ec77366f40bf824978ab8882a1538e4ce504
Barry Scott <barry@barrys-emacs.org>
2016-04-25 19:44:58 +0100

    Update to work with common

--- Source/wb-git.cmd
+++ Source/wb-git.cmd
@@ -1,8 +1,11 @@
 setlocal 
+set PYTHONPATH=%BUILDER_TOP_DIR%\Source;%BUILDER_TOP_DIR%\Source\Common
+
 set BUILDER_START_DIR=
 set BUILDER_TOP_DIR=
 set BUILDROOT_DIR=
 set __CMD_INIT_RUN__=
 
+echo PYTHONPATH %PYTHONPATH%
 %PYTHON% wb_git_main.py %* 
 endlocal 
