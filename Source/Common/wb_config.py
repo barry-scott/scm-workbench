e6544a2db1dd76d062c66f38b05c020a2b92bd50
Barry Scott <barry@barrys-emacs.org>
2016-06-18 12:21:50 +0100

    Common config items - face and pointsize to use as a default

---
+++ Source/Common/wb_config.py
@@ -0,0 +1,29 @@
+'''
+ ====================================================================
+ Copyright (c) 2003-2015 Barry A Scott.  All rights reserved.
+
+ This software is licensed as described in the file LICENSE.txt,
+ which you should have received as part of this distribution.
+
+ ====================================================================
+
+    be_config.py
+
+    Based on code from git WorkBench
+
+'''
+import sys
+
+# point size and face need to chosen for platform
+if sys.platform.startswith( 'win' ):
+    face = 'Courier New'
+    point_size = 8
+
+elif sys.platform == 'darwin':
+    face = 'Monaco'
+    point_size = 12
+
+else:
+    # for unix systems
+    face = 'Courier'
+    point_size = 12
