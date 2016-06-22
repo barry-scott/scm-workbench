b58418bc527414fa84515e9ac249afdc426dd955
Barry Scott <barry@barrys-emacs.org>
2016-06-22 16:16:12 +0100

    reorder functions into groups based on the scintilla documentation ordering need to use setIndicatorCurrent not setIndicatorValue to set indicators

--- Source/Common/wb_scintilla.py
+++ Source/Common/wb_scintilla.py
@@ -9,7 +9,7 @@
 
     wb_scintilla.py
 
-    proveide a nice python3 friendly wrapper arouns the raw Scintilla control
+    provide a nice python3 friendly wrapper arouns the raw Scintilla control
 
 '''
 from PyQt5 import Qsci
@@ -37,9 +37,91 @@ class WbScintilla(Qsci.QsciScintilla):
 
         self.SendScintilla( self.SCI_SETCODEPAGE, self.SC_CP_UTF8 )
 
-    def setProperty( self, prop, value ):
-        self.SendScintilla( self.SCI_SETPROPERTY, prop.encode( 'utf-8' ), value.encode( 'utf-8' ) )
+    # The following fucntion are organised in to section
+    # taken from the scintilla documentation.
+    # --- section-name ---
 
+    # --- Text retrieval and modification ---
+    def insertText( self, pos, text ):
+        self.SendScintilla( self.SCI_INSERTTEXT, pos, text.encode( 'utf-8' ) )
+
+    def replaceSel( self, text ):
+        self.SendScintilla( self.SCI_REPLACESEL, text.encode( 'utf-8' ) )
+
+    def setReadOnly( self, readonly ):
+        self.SendScintilla( self.SCI_SETREADONLY, readonly )
+
+    # --- Searching ---
+    # --- Overtype ---
+    # --- Cut, copy and paste ---
+    # --- Error handling ---
+    # --- Undo and Redo ---
+    def emptyUndoBuffer( self ):
+        self.SendScintilla( self.SCI_EMPTYUNDOBUFFER )
+
+    # --- Selection and information ---
+    def getLength( self ):
+        return self.SendScintilla( self.SCI_GETLENGTH )
+
+    def getLineCount( self ):
+        return self.SendScintilla( self.SCI_GETLINECOUNT )
+
+    def positionFromLine( self, line ):
+        return self.SendScintilla( self.SCI_POSITIONFROMLINE, line )
+
+    def getLineEndPosition( self, line ):
+        return self.SendScintilla( self.SCI_GETLINEENDPOSITION, line )
+
+    def setSelectionStart( self, start ):
+        self.SendScintilla( self.SCI_SETSELECTIONSTART, start )
+
+    def setSelectionEnd( self, end ):
+        self.SendScintilla( self.SCI_SETSELECTIONEND, end )
+
+    def lineFromPosition( self, pos ):
+        return self.SendScintilla( self.SCI_LINEFROMPOSITION, pos )
+
+    # --- Multiple Selection and Virtual Space ---
+    # --- Scrolling and automatic scrolling ---
+    def setFirstVisibleLine( self, line ):
+        self.SendScintilla( self.SCI_SETFIRSTVISIBLELINE, line )
+
+    def getFirstVisibleLine( self ):
+        return self.SendScintilla( self.SCI_GETFIRSTVISIBLELINE )
+
+    def gotoLine( self, line ):
+        self.SendScintilla( self.SCI_GOTOLINE, line )
+
+    def setScrollWidth( self, width ):
+        self.SendScintilla( self.SCI_SETSCROLLWIDTH, width )
+
+    def setHScrollBar( self, visible ):
+        self.SendScintilla( self.SCI_SETHSCROLLBAR, visible )
+
+    def setVScrollBar( self, visible ):
+        self.SendScintilla( self.SCI_SETVSCROLLBAR, visible )
+
+    # --- White space ---
+    def setViewWhiteSpace( self, visible ):
+        if visible:
+            self.setViewWs( self.SCWS_VISIBLEALWAYS )
+        else:
+            self.setViewWs( self.SCWS_INVISIBLE )
+
+    def setViewWs( self, mode ):
+        self.SendScintilla( self.SCI_SETVIEWWS, mode )
+
+    # --- Cursor ---
+    # --- Mouse capture ---
+    # --- Line endings ---
+    # --- Styling ---
+    def startStyling( self, pos ):
+        self.SendScintilla( self.SCI_STARTSTYLING, pos )
+
+    def setStyling( self, length, style_number ):
+        self.SendScintilla( self.SCI_SETSTYLING, length, style_number )
+
+    # --- Style definition ---
     def styleSetFromSpec( self, style_number, css ):
         # simple css-like style
         # name:value,name:value
@@ -61,53 +143,11 @@ class WbScintilla(Qsci.QsciScintilla):
             else:
                 assert False, 'Unknown name %r in css %r' % (name, css)
 
+    # --- Caret, selection, and hotspot styles ---
+    # --- Character representations ---
+    # --- Margins ---
     def setMarginWidth( self, margin, width ):
         self.SendScintilla( self.SCI_SETMARGINWIDTHN, margin, width )
-
-    def setScrollWidth( self, width ):
-        self.SendScintilla( self.SCI_SETSCROLLWIDTH, width )
-
-    def emptyUndoBuffer( self ):
-        self.SendScintilla( self.SCI_EMPTYUNDOBUFFER )
-
-    def getLength( self ):
-        return self.SendScintilla( self.SCI_GETLENGTH )
-
-    def insertText( self, pos, text ):
-        self.SendScintilla( self.SCI_INSERTTEXT, pos, text.encode( 'utf-8' ) )
-
-    def startStyling( self, pos ):
-        self.SendScintilla( self.SCI_STARTSTYLING, pos )
-
-    def setStyling( self, length, style_number ):
-        self.SendScintilla( self.SCI_SETSTYLING, length, style_number )
-
-    def positionFromLine( self, line ):
-        return self.SendScintilla( self.SCI_POSITIONFROMLINE, line )
-
-    def getLineEndPosition( self, line ):
-        return self.SendScintilla( self.SCI_GETLINEENDPOSITION, line )
-
-    def setSelectionStart( self, start ):
-        self.SendScintilla( self.SCI_SETSELECTIONSTART, start )
-
-    def setSelectionEnd( self, end ):
-        self.SendScintilla( self.SCI_SETSELECTIONEND, end )
-
-    def replaceSel( self, text ):
-        self.SendScintilla( self.SCI_REPLACESEL, text.encode( 'utf-8' ) )
-
-    def indicSetStyle( self, style_number, style ):
-        self.SendScintilla( self.SCI_INDICSETSTYLE, style_number, style )
-
-    def indicSetFore( self, style_number, color ):
-        self.SendScintilla( self.SCI_INDICSETFORE, style_number, parseColourToRgbInt( color ) )
-        
-    def setHScrollBar( self, visible ):
-        self.SendScintilla( self.SCI_SETHSCROLLBAR, visible )
-
-    def setVScrollBar( self, visible ):
-        self.SendScintilla( self.SCI_SETVSCROLLBAR, visible )
 
     def setMarginMask( self, margin, mask ):
         self.SendScintilla( self.SCI_SETMARGINMASKN, margin, mask )
@@ -115,6 +155,13 @@ class WbScintilla(Qsci.QsciScintilla):
     def setMarginSensitive( self, margin, sensitive ):
         self.SendScintilla( self.SCI_SETMARGINSENSITIVEN, margin, sensitive )
 
+
+
+    # --- Annotations ---
+    # --- Other settings ---
+    # --- Brace highlighting ---
+    # --- Tabs and Indentation Guides ---
+    # --- Markers ---
     def markerDefine( self, marker_number, marker_symbol ):
         self.SendScintilla( self.SCI_MARKERDEFINE, marker_number, marker_symbol )
 
@@ -124,15 +171,52 @@ class WbScintilla(Qsci.QsciScintilla):
     def markerSetBack( self, marker_number, colour ):
         self.SendScintilla( self.SCI_MARKERSETBACK, parseColourToRgbInt( colour ) )
 
-    def lineFromPosition( self, pos ):
-        return self.SendScintilla( self.SCI_LINEFROMPOSITION, pos )
+    # --- Indicators ---
+    def indicSetStyle( self, indic_number, style ):
+        self.SendScintilla( self.SCI_INDICSETSTYLE, indic_number, style )
 
-    def setReadOnly( self, readonly ):
-        self.SendScintilla( self.SCI_SETREADONLY, readonly )
+    def indicGetStyle( self, indic_number ):
+        return self.SendScintilla( self.SCI_INDICGETSTYLE, indic_number )
 
-    def getLength( self ):
-        return self.SendScintilla( self.SCI_GETLENGTH )
+    def indicSetFore( self, indic_number, color ):
+        self.SendScintilla( self.SCI_INDICSETFORE, indic_number, parseColourToRgbInt( color ) )
 
+    def indicGetFore( self, indic_number ):
+        return self.SendScintilla( self.SCI_INDICGETFORE, indic_number )
+
+    def setIndicatorValue( self, value ):
+        self.SendScintilla( self.SCI_SETINDICATORVALUE, value )
+
+    def getIndicatorValue( self ):
+        return self.SendScintilla( self.SCI_GETINDICATORVALUE )
+
+    def setIndicatorCurrent( self, indic_number ):
+        self.SendScintilla( self.SCI_SETINDICATORCURRENT, indic_number )
+
+    def getIndicatorCurrent( self ):
+        return self.SendScintilla( self.SCI_GETINDICATORCURRENT )
+
+    # use setIndicatorCurrent to set the indic_number to fill with
+    # note: setIndicatorValue does not work (docs are wrong)
+    def indicatorFillRange( self, position, fill_length ):
+        self.SendScintilla( self.SCI_INDICATORFILLRANGE, position, fill_length )
+
+    def indicatorClearRange( self, position, clear_length ):
+        self.SendScintilla( self.SCI_INDICATORCLEARRANGE, position, clear_length )
+
+    # --- OS X Find Indicator ---
+    # --- Autocompletion ---
+    # --- User lists ---
+    # --- Call tips ---
+    # --- Keyboard commands ---
+    # --- Key bindings ---
+    # --- Popup edit menu ---
+    # --- Macro recording ---
+    # --- Printing ---
+    # --- Direct access ---
+    # --- Multiple views ---
+    # --- Background loading and saving ---
+    # --- Folding ---
     def setFoldLevel( self, line, level ):
         self.SendScintilla( self.SCI_SETFOLDLEVEL, line, level )
 
@@ -145,42 +229,41 @@ class WbScintilla(Qsci.QsciScintilla):
     def getFoldExpanded( self, line ):
         return self.SendScintilla( self.SCI_GETFOLDEXPANDED, line )
 
-    def setZoom( self, zoom_in_points ):
-        self.SendScintilla( self.SCI_SETZOOM, zoom_in_points )
-
     def showLines( self, line_start, line_end ):
         self.SendScintilla( self.SCI_SHOWLINES, line_start, line_end ) 
 
     def hideLines( self, line_start, line_end ):
         self.SendScintilla( self.SCI_HIDELINES, line_start, line_end )
 
-    def getLineCount( self ):
-        return self.SendScintilla( self.SCI_GETLINECOUNT )
+    # --- Line wrapping ---
 
-    def setFirstVisibleLine( self, line ):
-        self.SendScintilla( self.SCI_SETFIRSTVISIBLELINE, line )
+    # --- Zooming ---
+    def setZoom( self, zoom_in_points ):
+        self.SendScintilla( self.SCI_SETZOOM, zoom_in_points )
 
-    def getFirstVisibleLine( self ):
-        return self.SendScintilla( self.SCI_GETFIRSTVISIBLELINE )
 
-    def gotoLine( self, line ):
-        self.SendScintilla( self.SCI_GOTOLINE, line )
+    # --- Long lines ---
 
-    def setViewWhiteSpace( self, visible ):
-        if visible:
-            self.setViewWs( self.SCWS_VISIBLEALWAYS )
-        else:
-            self.setViewWs( self.SCWS_INVISIBLE )
+    # --- Lexer ---
+    def setProperty( self, prop, value ):
+        self.SendScintilla( self.SCI_SETPROPERTY, prop.encode( 'utf-8' ), value.encode( 'utf-8' ) )
 
-    def setViewWs( self, mode ):
-        self.SendScintilla( self.SCI_SETVIEWWS, mode )
+    # ------------------------------------------------------------
 
     #--- high level API -------------------------------------------------------------
-    def insertStyledText( self, text, style ):
+    def insertStyledText( self, text, style_number, indic_number=None ):
+        # insert text and rember where
         pos = self.getLength()
         self.insertText( pos, text )
+
+        # apply a style to the text
         self.startStyling( pos )
-        self.setStyling( len(text), style )
+        self.setStyling( len(text), style_number )
+
+        if indic_number is not None:
+            # apply an indic_number to the text
+            self.setIndicatorCurrent( indic_number )
+            self.indicatorFillRange( pos, len(text) )
 
     def changeLineStyle( self, line, style ):
         pos_start = self.positionFromLine( line )
