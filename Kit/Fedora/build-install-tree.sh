483f51e769fc175ecc776782258a5d9c995d316d
Barry Scott <barry@barrys-emacs.org>
2016-07-27 17:14:00 +0100

    Fix up use of paths in build-install-tree to keep build tree and runtime tree names apart.

--- Kit/Fedora/build-install-tree.sh
+++ Kit/Fedora/build-install-tree.sh
@@ -2,24 +2,27 @@
 set -e
 
 echo "Info: build-install-tree.sh"
-BIN=${1? bin folder}
-LIB=${2? lib folder}
-MAN1=${2? man1 folder}
-DESKTOPFILES=${3? desktop files folder}
+# BUILD_ROOT is the build prefix e.g. /var/mock/xxx
+BUILD_ROOT=${1:? build root}
+# BIN, LIB, MAN1 and DESKTOPFILES are path on the target e.g. /usr/bin etc
+BIN=${2? bin folder}
+LIB=${3? lib folder}
+MAN1=${4? man1 folder}
+DESKTOPFILES=${5? desktop files folder}
 
-mkdir -p ${BIN} ${LIB} ${MAN1} ${DESKTOPFILES}
+mkdir -p ${BUILD_ROOT}${BIN} ${BUILD_ROOT}${LIB} ${BUILD_ROOT}${MAN1} ${BUILD_ROOT}${DESKTOPFILES}
 
-gzip -c ${BUILDER_TOP_DIR}/Kit/Fedora/scm-workbench.1 > ${MAN1}/scm-workbench.1.gz
-cp ${BUILDER_TOP_DIR}/Kit/Fedora/scm-workbench.desktop ${DESKTOPFILES}
+gzip -c ${BUILDER_TOP_DIR}/Kit/Fedora/scm-workbench.1 > ${BUILD_ROOT}${MAN1}/scm-workbench.1.gz
+cp ${BUILDER_TOP_DIR}/Kit/Fedora/scm-workbench.desktop ${BUILD_ROOT}${DESKTOPFILES}
 
-cat <<EOF >${BIN}/scm-workbench
+cat <<EOF >${BUILD_ROOT}${BIN}/scm-workbench
 #!/usr/bin/python3
 import sys
 sys.path.insert( 0, "${LIB}" )
 EOF
 
-cat ${BUILDER_TOP_DIR}/Source/Scm/wb_scm_main.py >>${BIN}/scm-workbench
-chmod +x ${BIN}/scm-workbench
+cat ${BUILDER_TOP_DIR}/Source/Scm/wb_scm_main.py >>${BUILD_ROOT}${BIN}/scm-workbench
+chmod +x ${BUILD_ROOT}${BIN}/scm-workbench
 
 pushd ${BUILDER_TOP_DIR}/Source/Scm
 make -f linux.mak
@@ -32,8 +35,8 @@ for LIBSRC in \
     ${BUILDER_TOP_DIR}/Source/Svn \
     ${BUILDER_TOP_DIR}/Source/Scm
 do
-    cp ${LIBSRC}/*.py ${LIB}
+    cp ${LIBSRC}/*.py ${BUILD_ROOT}${LIB}
 done
 
 # hack until GitPython is packaged by fedora
-cp -r ${BUILDER_TOP_DIR}/Import/GitPython/git ${LIB}/git
+cp -r ${BUILDER_TOP_DIR}/Import/GitPython/git ${BUILD_ROOT}${LIB}/git
