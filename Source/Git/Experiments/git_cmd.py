88a83e60c6e9f97d80dbd39120640fd1cfe1faf1
Barry Scott <barry@barrys-emacs.org>
2016-08-02 21:55:37 +0100

    Add windows version git cmd. - odd bug it does not work unless a print of text is added after the ReadFile for stdout.

--- Source/Git/Experiments/git_cmd.py
+++ Source/Git/Experiments/git_cmd.py
@@ -3,10 +3,15 @@
 #
 import sys
 import subprocess
-import selectors
-import ctypes
-import fcntl
-import os
+
+if sys.platform == 'win32':
+    import _winapi
+    import msvcrt
+
+else:
+    import selectors
+    import fcntl
+    import os
 
 class GitCmdUnix:
     def __init__( self, cmd, io_handlers ):
@@ -39,7 +44,7 @@ class GitCmdUnix:
                 self.key_stdin = self.selector.register( self.proc.stdin )
 
             draining = False
-            for key, events in self.selector.select( 0.1 ):
+            for key, events in self.selector.select( 0.01 ):
                 if key == self.key_stdout:
                     text = self.proc.stdout.read()
                     if len(text) == 0:
@@ -62,6 +67,8 @@ class GitCmdUnix:
                     self.proc.stdout.write( self.io_handlers._getInput().encode( sys.getdefaultencoding() ) )
                     if self.io_handlers._hasInput() and rc is None:
                         self.selector.unregister( self.proc.stdin )
+
+            self.io_handlers._dispatch()
 
         return rc
 
@@ -70,7 +77,11 @@ class GitCmdWindows:
         assert type(cmd) == list
         self.io_handlers = io_handlers
 
-        self.proc = subprocess.Popen( cmd, bufsize=0, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+        CREATE_NO_WINDOW = 0x08000000
+        self.proc = subprocess.Popen( cmd, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=CREATE_NO_WINDOW )
+        self.stdin = self.proc.stdin
+        self.stdout = self.proc.stdout
+        self.stderr = self.proc.stderr
 
     def dispatch( self ):
         rc = None
@@ -79,33 +90,52 @@ class GitCmdWindows:
             if rc is None:
                 rc = self.proc.poll()
 
-            if rc is None and self.key_stdin is not None and self.io_handlers._hasInput():
-                self.key_stdin = self.selector.register( self.proc.stdin )
+            all_fileobj = []
+            if self.stdout is not None:
+                all_fileobj.append( self.stdout )
+            if self.stderr is not None:
+                all_fileobj.append( self.stderr )
+
+            if rc is None and self.io_handlers._hasInput() and self.stdin is not None:
+                all_fileobj.append( self.stdio )
 
             draining = False
-            for key, events in self.selector.select( 0.1 ):
-                if key == self.key_stdout:
-                    text = self.proc.stdout.read()
-                    if len(text) == 0:
-                       self.selector.unregister( self.proc.stdout )
+            while rc is None or draining:
+                all_handles = [msvcrt.get_osfhandle( fileobj.fileno() ) for fileobj in all_fileobj]
+                result = _winapi.WaitForMultipleObjects( all_handles, 0, 10 )
+                print( 'result %r' % (result,) )
+                if result == _winapi.WAIT_TIMEOUT:
+                    break
 
-                    else:
-                        draining = True
-                        self.io_handlers._handleStdOut( text.decode( sys.getdefaultencoding() ) )
+                handle = all_handles[ result ]
+                fileobj = all_fileobj[ result ]
 
-                elif key == self.key_stderr:
-                    text = self.proc.stderr.read()
-                    if len(text) == 0:
-                       self.selector.unregister( self.proc.stderr )
+                if fileobj == self.stdout:
+                    try:
+                        text, err = _winapi.ReadFile( handle, 128, 0 )
 
-                    else:
-                        draining = True
-                        self.io_handlers._handleStdErr( text.decode( sys.getdefaultencoding() ) )
+                        if len(text) > 0:
+                            draining = True
+                            self.io_handlers._handleStdOut( text.decode( sys.getdefaultencoding() ) )
 
-                elif key == self.key_stdin:
-                    self.proc.stdout.write( self.io_handlers._getInput().encode( sys.getdefaultencoding() ) )
-                    if self.io_handlers._hasInput() and rc is None:
-                        self.selector.unregister( self.proc.stdin )
+                    except BrokenPipeError:
+                        self.stdout = None
+
+                elif fileobj == self.stderr:
+                    try:
+                        text, err = _winapi.ReadFile( handle, 1024, 0 )
+                        assert err == 0
+                        if len(text) > 0:
+                            draining = True
+                            self.io_handlers._handleStdErr( text.decode( sys.getdefaultencoding() ) )
+
+                    except BrokenPipeError:
+                        self.stderr = None
+
+                elif fileobj == self.stdin:
+                    written, err = _winapi.WriteFile( handle, self.io_handlers._getInput().encode( sys.getdefaultencoding() ), False )
+
+            self.io_handlers._dispatch()
 
         return rc
 
@@ -159,15 +189,23 @@ class GitIoHandler:
         return text
 
     def _handleStdOut( self, text ):
-        self.__handleOutput( text, self.__output, self.stdOutLine )
+        self.__output.addText( text )
 
     def _handleStdErr( self, text ):
-        self.__handleOutput( text, self.__error, self.stdErrLine )
+        self.__error.addText( text )
 
-    def __handleOutput( self, text, output, line_fn ):
-        output.addText( text )
+    def _dispatch( self ):
+        self.__dispatchOutput( self.__output, self.stdOutLine )
+        self.__dispatchOutput( self.__error, self.stdErrLine )
 
-        for delim in ('\r', '\n'):
+    if sys.platform == 'win32':
+        delim_list = ('\r\n', '\r', '\n')
+
+    else:
+        delim_list = ('\r', '\n')
+
+    def __dispatchOutput( self, output, line_fn ):
+        for delim in self.delim_list:
             while True:
                 line = output.getLine( delim )
                 if line is None:
@@ -186,8 +224,6 @@ class DebugGitIoHandler(GitIoHandler):
 
 if __name__ == '__main__':
     import sys
-    cmd = sys.argv[1:]
-
-    cmd = GitCmd( cmd, DebugGitIoHandler() )
+    cmd = GitCmd( sys.argv[1:], DebugGitIoHandler() )
     rc = cmd.dispatch()
     print( 'rc %r' % (rc,) )
