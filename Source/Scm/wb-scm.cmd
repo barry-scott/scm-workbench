d9d153a9d88f457ee8755af629b30789a1cba4f6
Barry Scott <barry@barrys-emacs.org>
2016-08-04 13:02:23 +0100

    build all assets before stating scm workbench

--- Source/Scm/wb-scm.cmd
+++ Source/Scm/wb-scm.cmd
@@ -5,6 +5,13 @@ if "%BUILDER_TOP_DIR%" == "" (
 )
 set PYTHONPATH=%BUILDER_TOP_DIR%\Source\Scm;%BUILDER_TOP_DIR%\Source\Common;%BUILDER_TOP_DIR%\Source\Git;%BUILDER_TOP_DIR%\Source\Svn;%BUILDER_TOP_DIR%\Source\Hg
 
+cd ..\Common
+%PYTHON% make_wb_diff_images.py
+
+cd ..\Scm
+nmake -f windows.mak clean
+nmake -f windows.mak all
+
 set BUILDER_START_DIR=
 set BUILDER_TOP_DIR=
 set BUILDROOT_DIR=
