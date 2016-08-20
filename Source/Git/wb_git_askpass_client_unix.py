d89ef28a24e86b9ec8fb3deb8303e3c0954c67ad
Barry Scott <barry@barrys-emacs.org>
2016-08-20 13:03:29 +0100

    allow to work on python2.7 and python3

--- Source/Git/wb_git_askpass_client_unix.py
+++ Source/Git/wb_git_askpass_client_unix.py
@@ -12,10 +12,14 @@
     called with argv[1:] as the prompt
     expects a single line output as response.
 
+
+    On macOS will be run by python2.7
+    On Linux we can use python3
+
 '''
 import sys
 import os
-import pathlib
+import os.path
 import pwd
 import stat
 import tempfile
@@ -30,17 +34,17 @@ class WbGitAskPass:
 
     def askPass( self, prompt ):
         e = pwd.getpwuid( os.geteuid() )
-        fifo_dir = pathlib.Path( tempfile.gettempdir() ) / e.pw_name
+        fifo_dir = os.path.join( tempfile.gettempdir(), e.pw_name )
 
-        client_fifo = fifo_dir / self.fifo_name_client
-        server_fifo = fifo_dir / self.fifo_name_server
+        client_fifo = os.path.join( fifo_dir, self.fifo_name_client )
+        server_fifo = os.path.join( fifo_dir, self.fifo_name_server )
 
         try:
             error_msg = 'Error: failed to open fifo - %s'
-            fd_server = os.open( str(server_fifo), os.O_WRONLY|os.O_NONBLOCK )
+            fd_server = os.open( server_fifo, os.O_WRONLY|os.O_NONBLOCK )
 
             error_msg = 'Error: failed to open fifo - %s'
-            fd_client = os.open( str(client_fifo), os.O_RDONLY|os.O_NONBLOCK )
+            fd_client = os.open( client_fifo, os.O_RDONLY|os.O_NONBLOCK )
 
             prompt = prompt.encode( 'utf-8' )
 
@@ -55,9 +59,8 @@ class WbGitAskPass:
 
             error_msg = 'Error: failed to read client fifo - %s'
 
-            poll = select.poll()
-            poll.register( fd_client, select.POLLIN )
-            poll.poll()
+            # wait for write to fd
+            select.select( [fd_client], [], [], None )
 
             reply = os.read( fd_client, 1024 )
 
