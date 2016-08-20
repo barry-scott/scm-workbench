93623a792f84dda07bcc0545b49507066db9438f
Barry Scott <barry@barrys-emacs.org>
2016-08-20 13:02:10 +0100

    put in a check for minimum python version and report clearly to the developer the problem

--- Source/Scm/wb_scm_main.py
+++ Source/Scm/wb_scm_main.py
@@ -12,10 +12,13 @@
 
 '''
 import sys
+MIN_SUPPORTED_PYTHON_VERSION = (3, 5)
+if sys.version_info < MIN_SUPPORTED_PYTHON_VERSION:
+    print( 'Error: Must be run using pthon %d.%d or newer' % MIN_SUPPORTED_PYTHON_VERSION )
+    sys.exit( 9 )
 
 import wb_main
 import wb_scm_app
-
 
 if __name__ == '__main__':
     sys.exit( wb_main.main( wb_scm_app.WbScmApp, sys.argv ) )
