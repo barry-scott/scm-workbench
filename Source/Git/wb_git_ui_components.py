5c7e444593a749ca09ba0084ac31b2a357ac599c
Barry Scott <barry@barrys-emacs.org>
2016-08-24 10:29:05 +0100

    Do not override the user's GIT_ASKPASS config.

--- Source/Git/wb_git_ui_components.py
+++ Source/Git/wb_git_ui_components.py
@@ -70,6 +70,10 @@ class GitMainWindowComponents(wb_git_ui_actions.GitMainWindowActions):
 
         self.log.info( 'Git using program %s' % (git.Git.GIT_PYTHON_GIT_EXECUTABLE,) )
 
+        if 'GIT_ASKPASS' in os.environ:
+            self.log.info( "Using user's GIT_ASKPASS program %s" % (os.environ[ 'GIT_ASKPASS' ],)
+            return
+
         self.askpass_server = wb_git_askpass_server.WbGitAskPassServer( self.app, self )
 
         devel_fallback = False
@@ -89,6 +93,7 @@ class GitMainWindowComponents(wb_git_ui_actions.GitMainWindowActions):
             return
 
         os.environ['GIT_ASKPASS'] = str(askpass)
+        self.log.info( "Using Workbench's GIT_ASKPASS program"
 
     def about( self ):
         if shutil.which( 'git' ) is None:
