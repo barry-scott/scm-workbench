9eb8d2250e4195e489aed32a8a3f4d881a07f34b
Barry Scott <barry@barrys-emacs.org>
2016-12-30 14:51:10 +0000

    After build the app clean out all unused files

--- Kit/Windows/build-app.cmd
+++ Kit/Windows/build-app.cmd
@@ -54,21 +54,87 @@ pushd %SRC_DIR%\Git
     if errorlevel 1 goto :error
 popd >NUL
 
-goto :eof
-echo Info: Remove unneeded files
-del %DIST_DIR%\PyWinAppRes\Lib\distutils\command\wininst-*.exe
-del %DIST_DIR%\PyWinAppRes\Lib\site-packages\pysvn\unins000.*
-rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\site-packages\PyQt5\Qt\qsci\api
-rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\ctypes\test
-rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\site-packages\git\test
-rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\site-packages\gitdb\test
-rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\site-packages\smmap\test
-rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\unittest\test
+rem rem rem rem 
 
-echo Info: build-app.cmd done
-goto :eof
+pushd %DIST_DIR%\PyWinAppRes\Lib\site-packages\PyQt5
+    if errorlevel 1 goto :error
+
+echo Info: clean up Qt 1. move all pyd and dll into a tmp folder
+mkdir tmp
+    if errorlevel 1 goto :error
+move Qt*.pyd tmp >NUL
+    if errorlevel 1 goto :error
+move tmp\Qt.pyd . >NUL
+    if errorlevel 1 goto :error
+
+mkdir Qt\bin\tmp
+    if errorlevel 1 goto :error
+move Qt\bin\Qt5*.dll Qt\bin\tmp >NUL
+    if errorlevel 1 goto :error
+
+echo Info: clean up Qt 2. bring back only the ones we use
+for %%x in (Core DBus Gui PrintSupport Svg Widgets) do call :qt_keep %%x
+
+echo Info: clean up Qt 3. delete the Qt files we do not need
+rmdir /s /q tmp
+    if errorlevel 1 goto :error
+rmdir /s /q Qt\bin\tmp
+    if errorlevel 1 goto :error
+
+echo Info: clean up Qt 4. delete qml file
+rmdir /s /q Qt\qml
+    if errorlevel 1 goto :error
+
+echo Info: clean up Qt 5. delete translations file
+rmdir /s /q Qt\translations
+    if errorlevel 1 goto :error
+
+echo Info: clean up Qt 6. delete webengine files
+del Qt\bin\QtWebEngineProcess.exe >NUL
+    if errorlevel 1 goto :error
+del Qt\resources\qtwebengine*.pak >NUL
+    if errorlevel 1 goto :error
+
+echo Info: clean up Qt 6. delete qsci resources 
+rmdir /s /q Qt\qsci
+    if errorlevel 1 goto :error
+
+
+echo Info: clean up python lib 1. delete test code
+rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\ctypes\test
+    if errorlevel 1 goto :error
+rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\site-packages\git\test
+    if errorlevel 1 goto :error
+rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\site-packages\gitdb\test
+    if errorlevel 1 goto :error
+rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\site-packages\smmap\test
+    if errorlevel 1 goto :error
+rmdir /s /q %DIST_DIR%\PyWinAppRes\Lib\unittest\test
+    if errorlevel 1 goto :error
+
+echo Info: clean up python lib 1. delete distutils exe
+del /s %DIST_DIR%\PyWinAppRes\Lib\distutils\command\wininst-*.exe
+    if errorlevel 1 goto :error
+
+echo Info: clean up python lib 1. delete pysvn uninstall exe
+del /s %DIST_DIR%\PyWinAppRes\Lib\site-packages\pysvn\unins000.exe
+    if errorlevel 1 goto :error
+
+popd
+
+echo Info: build-app completed successfully
+exit /b 0
+endlocal
+
+:qt_keep
+    echo Info: Keeping Qt%1
+    move tmp\Qt%1.pyd . >NUL
+        if errorlevel 1 goto :error
+    move Qt\bin\tmp\Qt5%1.dll Qt\bin >NUL
+        if errorlevel 1 goto :error
+    goto :eof
+
 :error
-    echo Error: Build failed.
+    echo Error: build-app failed
     exit /b 1
 
-endlocal
