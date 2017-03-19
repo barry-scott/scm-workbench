233f9ad5dd5c87c22af76c7b43e1a9369a5724d4
Barry Scott <barry@barrys-emacs.org>
2017-03-19 11:28:21 +0000

    update experiment into remote

--- Source/Git/Experiments/git_remotes.py
+++ Source/Git/Experiments/git_remotes.py
@@ -4,23 +4,24 @@ import sys
 import git
 
 r = git.Repo( sys.argv[1] )
-
-print( r.remotes )
-
+print( repr( r.remotes ) )
 for rem in r.remotes:
+    print( '=====' )
     for name in dir(rem):
         if name[0] != '_':
             v = getattr( rem, name )
-            #if not callable( v ):
-            print( 'remote %-15s: %r' % (name, v) )
+            if not callable( v ):
+                print( 'remote %-15s: %r' % (name, v) )
+    print( '􏼠remote.urls', list( rem.urls ) )
 
+    continue
 
     for ref in rem.refs:
+        print( '    -----' )
         for name in dir(ref):
             if name[0] != '_' and name not in ('ref','reference'):
                 v = getattr( ref, name )
-                #if not callable( v ):
-                print( 'ref %-15s: %r' % (name, getattr( ref, name )) )
-        print( '-----' )
+                if not callable( v ):
+                    print( '    ref %-15s: %r' % (name, getattr( ref, name )) )
         print( '    ref.name', ref.name )
         print( '    ref.commit', ref.commit )
