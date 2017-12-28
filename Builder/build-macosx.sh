79f4bdc4633aaad474a22c60cef7ea15691f031e
Barry Scott <barry@barrys-emacs.org>
2017-12-28 15:15:23 +0000

    Do not update the launcher database until a new method that works is discovered.

--- Builder/build-macosx.sh
+++ Builder/build-macosx.sh
@@ -2,11 +2,14 @@
 echo "Info: build-macosx.sh Builder - start"
 make -f macosx.mak PYTHON=$PYTHON clean build 2>&1 | tee build.log
 
-echo "Info: rebuild the OS X launch service database"
-CORE=/System/Library/Frameworks/CoreServices.framework/Versions/Current
-${CORE}/Frameworks/LaunchServices.framework/Versions/Current/Support/lsregister \
- -kill \
- -r -domain local -domain system -domain user 
+if /usr/bin/false
+then
+    echo "Info: rebuild the OS X launch service database"
+    CORE=/System/Library/Frameworks/CoreServices.framework/Versions/Current
+    ${CORE}/Frameworks/LaunchServices.framework/Versions/Current/Support/lsregister \
+     -kill \
+     -r -domain local -domain system -domain user 
+fi
 
 find .. -name '*.dmg'
 echo "Info: build-macosx.sh Builder - end"
