ebce0f7b24ac51094642c7e91f30e2c592aba102
Barry Scott <barry@barrys-emacs.org>
2018-06-12 09:52:32 +0100

    Add build script for non rpm build

---
+++ Kit/Fedora/build-linux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+KIT_DIR="${PWD}"
+BUILD="${KIT_DIR}/BUILD"
+BUILDROOT="${KIT_DIR}/BUILDROOT"
+
+./build-rpms.sh --tarball
+
+echo BUILD ${BUILD}
+
+rm -rf ${BUILD}
+mkdir ${BUILD}
+
+echo BUILDROOT ${BUILD}
+
+rm -rf ${BUILDROOT}
+mkdir ${BUILDROOT}
+
+cd ${BUILD}
+tar xf ../tmp/scm-workbench*.tar.gz
+ls -l
+cd scm-workbench-*
+. ~/bin/builder_init
+${KIT_DIR}/build-install-tree.sh \
+    "${BUILDROOT}" \
+    "$(rpm --eval %{_bindir})" \
+    "$(rpm --eval %{_datarootdir})/scm-workbench/lib" \
+    "$(rpm --eval %{_mandir})/man1" \
+    "$(rpm --eval %{_datarootdir})/doc/scm-workbench" \
+    "$(rpm --eval %{_datarootdir})/applications"
