dc0742d8a2e3f101d19627d24ebc8cda4fcdcd24
Barry Scott <barry@barrys-emacs.org>
2018-06-12 09:56:38 +0100

    Kit not Kits

--- Kit/Fedora/local-install.sh
+++ Kit/Fedora/local-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ${BUILDER_TOP_DIR?run builder_init}/Kits/Fedora/BUILDROOT
+cd ${BUILDER_TOP_DIR?run builder_init}/Kit/Fedora/BUILDROOT
 
 tar czf ${TMPDIR:-/tmp}/scm-workbench-tmp.tar.gz usr
 sudo tar xzf ${TMPDIR:-/tmp}/scm-workbench-tmp.tar.gz -C /
