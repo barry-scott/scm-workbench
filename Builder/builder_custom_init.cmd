d41d3b6726557ee440725574b0b0ca9c8b93d3e3
Barry Scott <barry@barrys-emacs.org>
2018-11-09 12:57:45 +0000

    Test for install location of the Visual Studio files

--- Builder/builder_custom_init.cmd
+++ Builder/builder_custom_init.cmd
@@ -11,9 +11,14 @@ set PYTHON_FILE_VERSION=36
 
 rem if Win64 then setup path to include the 64bit CL.exe
 rem when called in place this can fork bomb (lots of CMD.exe in task manager)
-call "C:\Program Files (x86)\Microsoft Visual Studio %VC_VER%\VC\bin\amd64\vcvars64.bat"
+if exist "C:\Program Files (x86)\Microsoft Visual Studio %VC_VER%\VC\bin\amd64\vcvars64.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio %VC_VER%\VC\bin\amd64\vcvars64.bat"
+)
+if exist "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
+    call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
 @echo %__e%
 
 set PYTHON=c:\python%PYTHON_FILE_VERSION%.%BUILDER_CFG_PLATFORM%\python
-PATH c:\python%PYTHON_FILE_VERSION%.%BUILDER_CFG_PLATFORM%;%PATH%
+PATH c:\python%PYTHON_FILE_VERSION%.%BUILDER_CFG_PLATFORM%;"c:\Program Files\gettext-iconv\bin";%PATH%
 %PYTHON% -c "import sys;print( 'Python:', sys.version )"
