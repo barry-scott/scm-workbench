b2de2fa25b9813571937d6e9748425d1d70aeeb6
Barry Scott <barry@barrys-emacs.org>
2018-12-01 10:22:52 +0000

    fix white space

--- Source/Scm/wb_scm_project_dialogs.py
+++ Source/Scm/wb_scm_project_dialogs.py
@@ -30,7 +30,7 @@ class WbScmAddProjectWizard(QtWidgets.QWizard):
         self.all_factories = app.all_factories
         super().__init__()
 
-        self.setWizardStyle(self.ClassicStyle)
+        self.setWizardStyle( self.ClassicStyle )
 
         self.setWindowTitle( T_('Add Project Wizard - %s') % (' '.join( app.app_name_parts ),) )
 
