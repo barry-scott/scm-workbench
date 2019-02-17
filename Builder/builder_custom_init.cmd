c2ea2766b617525d6fac7a21fec42f2bb17d1b66
Barry Scott <barry@barrys-emacs.org>
2019-02-17 14:19:32 +0000

    Remove the need to have nmake (from Microsoft Visual Studio) Make wb-scm.cmd to all that is necessary to get wb-scm running from the command line. If xgettext is installed then use it to create the translation files.

--- Builder/builder_custom_init.cmd
+++ Builder/builder_custom_init.cmd
@@ -9,16 +9,6 @@ set BUILDER_CFG_BUILD_TYPE=Release
 set PYTHON_VERSION=3.7
 set PYTHON_FILE_VERSION=37
 
-rem if Win64 then setup path to include the 64bit CL.exe
-rem when called in place this can fork bomb (lots of CMD.exe in task manager)
-if exist "C:\Program Files (x86)\Microsoft Visual Studio %VC_VER%\VC\bin\amd64\vcvars64.bat" (
-    call "C:\Program Files (x86)\Microsoft Visual Studio %VC_VER%\VC\bin\amd64\vcvars64.bat"
-)
-if exist "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
-    call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-)
-@echo %__e%
-
-set PYTHON=c:\python%PYTHON_FILE_VERSION%.%BUILDER_CFG_PLATFORM%\python
-PATH c:\python%PYTHON_FILE_VERSION%.%BUILDER_CFG_PLATFORM%;c:\Program Files\gettext-iconv\bin;%PATH%
+for /f "usebackq" %%X in (`py -%PYTHON_VERSION%-64 -c "import sys;print(sys.executable)"`) do set PYTHON=%%X
+echo Python %PYTHON_VERSION% found in %PYTHON%
 %PYTHON% -c "import sys;print( 'Python:', sys.version )"
--- Source/Scm/wb-scm.cmd
+++ Source/Scm/wb-scm.cmd
@@ -1,10 +1,29 @@
 @echo off
 setlocal
 if "%BUILDER_TOP_DIR%" == "" (
-    echo Error: BUILDER_TOP_DIR is not set
-    echo Make sure Builder\builder_custom_init.cmd has been run
-    goto :eof
+    echo Info: Bootstrap the build environment
+    set BUILDER_START_DIR=%CD%
+    :scan
+        if exist Builder goto found
+        for %%I in (%CD%) do set __builder_tmp=%%~pI
+        if "%__builder_tmp%" == "\" goto not_found
+        cd ..
+        goto scan
+
+    :not_found
+        echo Error: Cannot find the Builder folder
+        goto :eof
+
+    :found
+        set __builder_tmp=
+        set BUILDER_TOP_DIR=%CD%
+
+        cd Builder
+        echo Info: Builder running builder_custom_init.cmd
+        call builder_custom_init.cmd
+        cd %BUILDER_START_DIR%
 )
+
 if "%PYTHON%" == "" (
     echo Error: PYTHON is not set
     echo Make sure Builder\builder_custom_init.cmd has been run
@@ -14,15 +33,31 @@ if "%PYTHON%" == "" (
 set PYTHONPATH=%BUILDER_TOP_DIR%\Source\Scm;%BUILDER_TOP_DIR%\Source\Common;%BUILDER_TOP_DIR%\Source\Git;%BUILDER_TOP_DIR%\Source\Svn;%BUILDER_TOP_DIR%\Source\Hg
 
 cd ..\Common
-%PYTHON% make_wb_diff_images.py
+    %PYTHON% make_wb_diff_images.py
     if errorlevel 1 goto :eof
 
 cd ..\Scm
-nmake -f windows.mak clean
-    if errorlevel 1 goto :eof
-nmake -f windows.mak all
-    if errorlevel 1 goto :eof
+echo Info: Build Clean
+    if exist locale rmdir /s /q locale
+    echo zzz >I18N\zzzqqqzzz.current.po
+    del I18N\*.current.po
+    if exist wb_scm_images.py del wb_scm_images.py
+    if exist wb_scm_version.py del wb_scm_version.py
 
+echo Info: Build all
+    %PYTHON% -u make_wb_scm_images.py
+    %PYTHON% -u make_wb_scm_version.py %BUILDER_TOP_DIR%/Builder/version.dat wb_scm_version.py
+    if not exist locale\en\LC_MESSAGES mkdir locale\en\LC_MESSAGES
+    for /F "usebackq" %%X in ('xgettext.exe') do set XGETTXT_PATH=MISSING%%~$PATH:X
+    if "%XGETTXT_PATH%" == "MISSING" (
+        echo Warning: cannot make translation MO files - cannot find xgettext.exe
+    ) else (
+        cd I18N
+        %PYTHON% make_pot_file.py
+        %PYTHON% make_po_file.py en
+        %PYTHON% make_mo_files.py ..\locale
+        cd ..
+    )
 set PROG=scm_workbench_git_callback.cmd
 echo "%PYTHON%" "%BUILDER_TOP_DIR%\Source\Git\wb_git_callback_client_win32.py" %%1 %%2 >%PROG%
 
@@ -30,7 +65,6 @@ set BUILDER_START_DIR=
 set BUILDER_TOP_DIR=
 set BUILDROOT_DIR=
 
-echo on
 echo PYTHONPATH %PYTHONPATH%
 %PYTHON% wb_scm_main.py %*
 if errorlevel 1 echo Error: wb_scm_main exited with error
