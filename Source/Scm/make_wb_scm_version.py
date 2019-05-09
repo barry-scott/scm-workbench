e7ff7b35ad47336e5d951dab23535eb4de4a6009
Barry Scott <barry@barrys-emacs.org>
2019-05-09 19:00:52 +0100

    replace build-rpms.sh with a python script that is easier to maintain.
    Allow the spec file to have a specified release.
    Change the arch to noarch.

--- Source/Scm/make_wb_scm_version.py
+++ Source/Scm/make_wb_scm_version.py
@@ -1,23 +1,27 @@
 import sys
 import subprocess
 
-v = {}
+def main( argv ):
+    v = {}
 
-with open( sys.argv[1], 'r' ) as f:
-    for line in f:
-        line = line.strip()
-        if line == '':
-            continue
+    with open( argv[1], 'r' ) as f:
+        for line in f:
+            line = line.strip()
+            if line == '':
+                continue
 
-        if line.startswith( '#' ):
-            continue
+            if line.startswith( '#' ):
+                continue
 
-        key, value = line.split( '=', 1 )
-        v[ key.strip() ] = value.strip()
+            key, value = line.split( '=', 1 )
+            v[ key.strip() ] = value.strip()
 
-result = subprocess.run( ['git', 'show-ref', '--head', '--hash', 'head'], stdout=subprocess.PIPE )
+    result = subprocess.run( ['git', 'show-ref', '--head', '--hash', 'head'], stdout=subprocess.PIPE )
 
-with open( sys.argv[2], 'w' ) as f:
-    f.write( 'commit="%s"\n' % (result.stdout.decode('utf-8').strip(),) )
-    for key in sorted( v.keys() ):
-        f.write( '%s=%s\n' % (key, v[ key ]) )
+    with open( argv[2], 'w' ) as f:
+        f.write( 'commit="%s"\n' % (result.stdout.decode('utf-8').strip(),) )
+        for key in sorted( v.keys() ):
+            f.write( '%s=%s\n' % (key, v[ key ]) )
+
+if __name__ == '__main__':
+    sys.exit( main( sys.argv ) )
