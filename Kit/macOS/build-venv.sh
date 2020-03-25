0203d6e34700c6ca61904fc36e257db4f0987a44
Barry Scott <barry@barrys-emacs.org>
2020-03-25 16:02:06 +0000

    Update to python 3.8 and latest dependencies.

--- Kit/macOS/build-venv.sh
+++ Kit/macOS/build-venv.sh
@@ -3,14 +3,16 @@
 colour-print "<>info Info: <> Clean up"
 rm -rf venv.tmp
 
-colour-print "<>info Info: <> Create venv"
+colour-print "<>info Info: <> Create venv for $PYTHON"
 $PYTHON -m venv venv.tmp
 
 colour-print "<>info Info: <> Install requirements"
+
+venv.tmp/bin/python -m pip install --upgrade pip setuptools
 venv.tmp/bin/python -m pip install -r requirements.txt
 venv.tmp/bin/python -m pip list
 
 colour-print "<>info Info: <> Install pysvn"
 PYSVN_ORIGIN=$($PYTHON -c 'import os,pysvn;print(os.path.dirname(pysvn.__file__))')
-cp -r ${PYSVN_ORIGIN} venv.tmp/lib/python3.7/site-packages/
+cp -r ${PYSVN_ORIGIN} venv.tmp/lib/python${PYTHON_VERSION}/site-packages/
 ./venv.tmp/bin/python -c 'import pysvn;print( "Info: PySVN %r SVN %r" % (pysvn.version, pysvn.svn_version) )'
