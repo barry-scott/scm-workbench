c8003bc5a2e88972b5e9170f218f295736bde036
Barry Scott <barry@barrys-emacs.org>
2020-07-18 11:42:16 +0100

    Make the rotate log code open the log with the same encoding used when the original log files was opened. This may be the cause of the logging Traceback reported in https://github.com/barry-scott/scm-workbench/issues/15 where the log file appears to have been opened with an encoding of 'ascii'.
    The rotate code was not consistent with log file naming. Meaning that it only
    keep 1 extra log.
    
    Fix the commits for the rotate code to match the algorithm used.

--- Source/Common/wb_logging.py
+++ Source/Common/wb_logging.py
@@ -386,30 +386,32 @@ class WbLogTextWidget(QtWidgets.QTextEdit):
 class RotatingFileHandler(logging.FileHandler):
     def __init__(self, filename, mode="a", maxBytes=0, backupCount=0):
         """
+
         Open the specified file and use it as the stream for logging.
 
-        By default, the file grows indefinitely. You can specify particular
-        values of maxBytes and backupCount to allow the file to rollover at
-        a predetermined size.
+        By default, the file grows indefinitely.  You can specify
+        particular values of maxBytes and backupCount to allow the file
+        to rollover at a predetermined size.
 
-        Rollover occurs whenever the current log file is nearly maxBytes in
-        length. If backupCount is >= 1, the system will successively create
-        new files with the same pathname as the base file, but with extensions
-        ".1", ".2" etc. appended to it. For example, with a backupCount of 5
-        and a base file name of "app.log", you would get "app.log",
-        "app.log.1", "app.log.2", ... through to "app.log.5". The file being
-        written to is always "app.log" - when it gets filled up, it is closed
-        and renamed to "app.log.1", and if files "app.log.1", "app.log.2" etc.
-        exist, then they are renamed to "app.log.2", "app.log.3" etc.
-        respectively.
+        Rollover occurs whenever the current log file is nearly
+        maxBytes in length.  If backupCount is >= 1, the system will
+        successively create new files with the same pathname as the
+        base file, but add in ".1", ".2" etc.
+
+        The file being written to is always "app.log" - when it gets
+        filled up, it is closed and renamed to "app.1.log", and if
+        files "app.1.log", "app.2.log" etc.  exist, then they are
+        renamed to "app.2.log", "app.3.log" etc.  respectively.
+
+        For example, with a backupCount of 5 and a base file name of
+        "app.log", you would get "app.log", "app.1.log", "app.2.log",
+        ...  through to "app.5.log".
 
         If maxBytes is zero, rollover never occurs.
         """
-        logging.FileHandler.__init__(self, filename, mode)
+        super().__init__(filename, mode, encoding='utf-8')
         self.maxBytes = maxBytes
         self.backupCount = backupCount
-        if maxBytes > 0:
-            self.mode = "a"
 
     def doRollover(self):
         """
@@ -426,12 +428,12 @@ class RotatingFileHandler(logging.FileHandler):
                     if os.path.exists(dfn):
                         os.remove(dfn)
                     os.rename(sfn, dfn)
-            dfn = self.baseFilename + ".1"
+            dfn = "%s.%d%s" % (prefix, 1, suffix)
             if os.path.exists(dfn):
                 os.remove(dfn)
             os.rename(self.baseFilename, dfn)
 
-        self.stream = open(self.baseFilename, "w")
+        self.stream = open(self.baseFilename, self.mode, encoding=self.encoding)
 
     def emit(self, record):
         """
