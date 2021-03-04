af9c559a5ba321c6f305026df6b54429ecbf938f
Barry Scott <barry@barrys-emacs.org>
2021-03-04 20:41:36 +0000

    On Fedora we do not use a venv as most of the pieces are packaged for the OS

--- Source/Scm/wb-scm.sh
+++ Source/Scm/wb-scm.sh
@@ -77,7 +77,7 @@ else
         PATH=/usr/bin:/bin:/usr/sbin:/sbin ${BUILDER_TOP_DIR}/Builder/tmp/venv/bin/python wb_scm_main.py "$@"
         ;;
     *)
-        ${BUILDER_TOP_DIR}/Builder/venv.tmp/bin/python wb_scm_main.py "$@"
+        python3 wb_scm_main.py "$@"
         ;;
     esac
 fi
