9a34f6c6d4b78a9f4c3a57c6f7049c5fe21a04c1
Barry Scott <barry@barrys-emacs.org>
2022-12-20 20:05:39 +0000

    Fix scintilla package name for PyQt6 default to python 3.11

--- Builder/builder_custom_init.sh
+++ Builder/builder_custom_init.sh
@@ -4,13 +4,13 @@ export BUILDER_CFG_PLATFORM=$(uname -s)
 case ${BUILDER_CFG_PLATFORM} in
 
 Darwin)
-    export PYTHON_VERSION=${1:-3.10}
+    export PYTHON_VERSION=${1:-3.11}
     export BUILDER_CFG_PLATFORM=MacOSX
     export PYTHON=python${PYTHON_VERSION}
     ;;
 
 Linux)
-    export PYTHON_VERSION=${1:-3.10}
+    export PYTHON_VERSION=${1:-3.11}
 
     for version in ${PYTHON_VERSION} 3.10 3.9 3.8
     do
@@ -50,7 +50,7 @@ Linux)
     fi
     ;;
 *)
-    export PYTHON_VERSION=${1:-3.10}
+    export PYTHON_VERSION=${1:-3.11}
     # no need to change
     ;;
 esac
