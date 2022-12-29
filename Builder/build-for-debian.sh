107a1cb1ae865cd326aa584cebbe895bbb780504
Barry Scott <barry@barrys-emacs.org>
2022-12-29 17:11:31 +0000

    auto increment release on each debian build

--- Builder/build-for-debian.sh
+++ Builder/build-for-debian.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+release=$(( $(<debian_release.txt) + 1 ))
+echo $release >debian_release.txt
+
 python3 ./package_workbench.py debian-source \
-    --release=1 \
+    --release=${release} \
     --colour
