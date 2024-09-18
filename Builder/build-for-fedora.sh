8031d3be24a9e6ae3275cf5961cac327bc5226ed
Barry Scott <barry@barrys-emacs.org>
2024-09-18 11:55:20 +0100

    Update build-for-fedora.sh to be consistent with other projects

--- Builder/build-for-fedora.sh
+++ Builder/build-for-fedora.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+set -e
+
+function usage() {
+    cat <<EOF
+$0 copr-release|copr-testing <fedora-version> [<arch>]
+EOF
+}
+
 cmd=${1:-copr-testing}
 rel=${2}
+case "$3" in
+x86_64|aarch64):
+    ARCH=${3}
+    ;;
+
+"")
+    ARCH=$(arch)
+    ;;
+
+*)
+    usage
+    exit 1
+    ;;
+esac
+
 if [ "${rel}" = "" ]
 then
     python3 ./package_workbench.py ${cmd} \
@@ -8,6 +31,6 @@ then
 
 else
     python3 ./package_workbench.py ${cmd} \
-        --mock-target=fedora-${rel}-$(arch) \
+        --mock-target=fedora-${rel}-${ARCH} \
         --colour
 fi
